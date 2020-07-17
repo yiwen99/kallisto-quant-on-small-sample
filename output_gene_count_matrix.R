d5eb00f8f4826e886ffac599d1b0571a315be47a
Yiwen Chen <50638410+yiwen99@users.noreply.github.com>
2020-07-17 08:04:10 +0800

    Update output_gene_count_matrix.R

--- output_gene_count_matrix.R
+++ output_gene_count_matrix.R
@@ -7,24 +7,21 @@ samples = list.files(common_path)
 
 files <- file.path(common_path,samples,"abundance.h5")
 
-names(files)<-paste0("sample",1:87)
+names(files)<-paste0(samples)
 
 to_gene <- read.delim(file.path("/u/project/zarlab/hjp/geuvadis_data/annotation", "transcripts_to_genes.txt"),header=FALSE)
 #to_gene is now a 3-col matrix, need to make tx2gene a 2-column matrix
 
 #make a 2-col transcript to gene matrix
-first<-to_gene[,c(1,3)]
-second<-to_gene[,c(2,3)]
-
-#change the column names of the two so that they can be bind together using rbind
-colnames(first)<-c("V1","V2")
-colnames(second)<-c("V1","V2")
-
-tx2gene <- rbind(first,second)
+#the second column is the ensembl gene name
+tx2gene<-to_gene[,c(1,2)]
+tx2gene$V2<-tx2gene$V2[1:11]
 head(tx2gene)
 
-txi.kallisto <- tximport(files, type = "kallisto", tx2gene=tx2gene, txOut = FALSE)
+txi.kallisto <- tximport(files, type = "kallisto",countsFromAbundance = "scaledTPM", tx2gene=tx2gene, txOut = FALSE)
 count_matrix <- txi.kallisto$counts
 head(count_matrix)
+
 write.table(count_matrix, file="COUNTS.txt", append = FALSE, sep = "\t", dec = ".", row.names = TRUE, col.names = TRUE)
+
 
